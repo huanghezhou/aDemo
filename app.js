084e3f0bf6c7e8a54e3df4105b71bde2c4fed708
HuangHeZhou <2691098108@qq.com>
2019-11-29 11:23:34 +0800

    md file change

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const http = require("http");
-const fs = require("fs");
-const A = require("./module/a");
+const http = require('http');
+const fs = require('fs');
+const A = require('./module/a');
 
 const server = http.createServer();
 
-server.on("request", (req, res) => {
+server.on('request', (req, res) => {
   console.log(req.socket.remoteAddress);
   console.log(req.socket.remotePort);
   console.log(A.testBC());
@@ -12,26 +12,26 @@ server.on("request", (req, res) => {
   // res.end("测试");
 
   let url = req.url;
-  if (url === "/a") {
-    res.setHeader("Content-type", "text/plain;charset=utf-8");
+  if (url === '/a') {
+    res.setHeader('Content-type', 'text/plain;charset=utf-8');
     res.end("文本测试")
-  } else if (url === "/img1") {
-    fs.readFile("./img/a.jpg", (err, data) => {
+  } else if (url === '/img1') {
+    fs.readFile('./img/a.jpg', (err, data) => {
       if (err) {
-        res.setHeader("Content-type", "text/plain;charset=utf-8");
-        res.end("图片加载失败，请稍后重试")
+        res.setHeader('Content-type', 'text/plain;charset=utf-8');
+        res.end('图片加载失败，请稍后重试')
       } else {
-        res.setHeader("Content-type", "image/jpeg");
+        res.setHeader('Content-type", "image/jpeg');
         res.end(data);
       }
     })
-  } else if (url === "/html") {
-    fs.readFile("./module/index.html", (err, data) => {
+  } else if (url === '/html') {
+    fs.readFile('./module/index.html', (err, data) => {
       if (err) {
-        res.setHeader("Content-type", "text/plain;charset=utf-8");
-        res.end("html加载失败")
+        res.setHeader('Content-type", "text/plain;charset=utf-8');
+        res.end('html加载失败')
       } else {
-        res.setHeader("Content-type", "text/html;charset=utf-8");
+        res.setHeader('Content-type", "text/html;charset=utf-8');
         res.end(data);
       }
     })
@@ -39,5 +39,5 @@ server.on("request", (req, res) => {
 });
 
 server.listen(3000, () => {
-  console.log("server is running...   http://127.0.0.1:3000");
+  console.log('server is running...   http://127.0.0.1:3000');
 })
--- instructions.md
+++ instructions.md
@@ -1,15 +1,19 @@
 # nodeJS require和end
 
 - 说明：
-+ $node app.js 运行项目，使用 http://127.0.0.1:3000/a， http://127.0.0.1:3000/img1， http://127.0.0.1:3000/html查看变化
++ $node app.js 运行项目，使用 http://127.0.0.1:3000/a， http://127.0.0.1:3000/img1， http://127.0.0.1:3000/html 查看变化
 
 ## require两个特点
-    - require有两个功能
-    + 1、加载并执行文件模块
-    + 2、获取文件模块的 exports对象
+- require有两个功能
++ 1、加载并执行文件模块
++ 2、获取文件模块的 exports对象
 
-    - node中没有全局作用于，只有文件模块作用于
-    + 把b.js require到a.js，把c.js require到b.js，a.js就可以拥有b.js和c.js的功能
+- node中没有全局作用于，只有文件模块作用于
++ 把b.js require到a.js，把c.js require到b.js，a.js就可以拥有b.js和c.js的功能
 
 ## end()的特点
-    - res.end()可以处理二进制和文本字符串
+- res.end()可以处理二进制和文本字符串
+```javascript
+res.setHeader('Content-type', 'text/plain; charset = utf-8')
+```
++ Content-type 告诉浏览器，服务端响应的是普通文本，响应的数据类型为utf-8
