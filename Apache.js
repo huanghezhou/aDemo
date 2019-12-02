563eae992915d8e13ebda42b8072477c429be3bc
HuangHeZhou <2691098108@qq.com>
2019-12-02 14:58:57 +0800

    实现一个简单的Apache服务器功能

--- Apache.js
+++ Apache.js
@@ -1,14 +1,30 @@
 const http = require('http')
 const fs = require('fs')
+const path = require('path')
 // 1、创建server
 const server = http.createServer()
 
 // 2、给server绑定 request 事件，并添加处理函数
 server.on('request', (req, res)=>{
-  console.log(req.socket.remoteAddress)
-  console.log(req.socket.remotePort)
-  console.log(req.url)
-  res.end('Hello Apache')
+  // console.log(req.socket.remoteAddress)
+  // console.log(req.socket.remotePort)
+  // console.log(req.url)
+  // res.end('Hello Apache')
+
+  let url = req.url
+  let fileUrl = path.resolve(__dirname)+'/www'
+  let page = '/index.html'
+  if (url !== '/') {
+    page = url
+  }
+  fs.readFile(fileUrl + page, (err, data) => {
+    if (err) {
+      return res.end('404 Not found')
+    }
+    console.log(data.toString())
+    res.end(data)
+  })
+  
 })
 
 // 3、监听 8080端口
